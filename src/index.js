48eaeb2ba9b261b570adebc4902aed93aa0a7aa8
italobfa <italobfa@hotmail.com>
2021-11-11 22:22:46 -0300

    add newmethods

--- src/index.js
+++ src/index.js
@@ -2,8 +2,24 @@ const express = require("express");
 
 const app = express();
 
-app.get("/", (request, response) => {
-  return response.json({ message: "Hello World" });
+app.get("/movies", (request, response) => {
+  return response.json([ "filme 1", "filme 2", "filme 3"]);
 });
+
+app.post("/movies", (request, response) => {
+  return response.json([ "filme 1", "filme 2", "filme 3", "filme 4"]);
+})
+
+app.put("/movies/:id", (request, response) => {
+  return response.json([ "filme 1", "filme 2", "filme 3", "filme 5"]);
+})
+
+app.patch("/movies/:id", (request, response) => {
+  return response.json([ "filme 1", "filme 2", "filme 4", "filme 5"]);
+})
+
+app.delete("/movies/:id", (request, response)=> {
+  return response.json([ "filme 1", "filme 2", "filme 4"]);
+})
 
 app.listen(3333);
